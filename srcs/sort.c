20c7f024d1219c7374636ec3d403b4a79cf9ca9b
ZorkZ <alexandre.stachnik2@gmail.com>
2023-02-07 17:23:40 +0100

    add sort 5 revers a fix

--- srcs/sort.c
+++ srcs/sort.c
@@ -6,11 +6,27 @@
 /*   By: astachni <astachni@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 14:35:25 by astachni          #+#    #+#             */
-/*   Updated: 2023/02/07 17:07:58 by astachni         ###   ########.fr       */
+/*   Updated: 2023/02/07 17:20:20 by astachni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/push_swap.h"
+
+// void	take_a_to_b(t_list **stack_a, t_list **stack_b)
+// {
+// 	ssize_t	min;
+
+// 	min = min_pos(*stack_a);
+// 	if (min == 3 || min == 2)
+// 	{
+// 		if (min == 3)
+// 			rotate(stack_a, 'a');
+// 		swap(stack_a, 'a');
+// 	}
+// 	if (min == 4)
+// 		reverse (stack_a, 'a');
+// 	push(stack_b, stack_a, 'b');
+// }
 
 void	sort_3(t_list **stack_a)
 {
@@ -45,17 +61,36 @@ void	sort_4(t_list **stack_a, t_list **stack_b)
 	push(stack_a, stack_b, 'a');
 }
 
+void	sort_5(t_list **stack_a, t_list **stack_b)
+{
+	ssize_t	min;
+
+	min = min_pos(*stack_a);
+	if (min == 3 || min == 2)
+	{
+		if (min == 3)
+			rotate(stack_a, 'a');
+		swap(stack_a, 'a');
+	}
+	if (min == 4 || min == 5)
+	{
+		if (min == 4)
+			reverse (stack_a, 'a');
+		reverse (stack_a, 'a');
+	}
+	push(stack_b, stack_a, 'b');
+	sort_4(stack_a, stack_b);
+}
+
 void	sort(t_list **stack_a, t_list **stack_b)
 {
 	int		size_a;
 
 	size_a = ft_lstsize(*stack_a);
 	if (size_a == 3)
-	{
 		sort_3(stack_a);
-	}
-	if (size_a == 4)
-	{
+	else if (size_a == 4)
 		sort_4(stack_a, stack_b);
-	}
+	else if (size_a == 5)
+		sort_5(stack_a, stack_b);
 }
